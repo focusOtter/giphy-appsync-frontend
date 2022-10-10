276f619b6b2a8f72f7d3861c20f771e2f806e3e8
Michael Liendo <mtliendo@amazon.com>
2022-10-10 17:03:36 -0500

    init setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { AmplifyProvider } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<AmplifyProvider>
+			<Component {...pageProps} />
+		</AmplifyProvider>
+	)
 }
 
 export default MyApp
